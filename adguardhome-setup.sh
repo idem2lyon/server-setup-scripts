fac9c11166c6958dbeda4d435e3ee7b3ba25912f
Idem <mehdi@famille-hamida.fr>
2025-01-16 09:56:13 +0100

    Update adguardhome-setup.sh

--- adguardhome-setup.sh
+++ adguardhome-setup.sh
@@ -5,7 +5,10 @@
 #   https://github.com/idem2lyon/server-setup-scripts/blob/main/adguardhome-setup.sh
 #
 # You can install it quickly using:
-#   curl -s -S -L https://raw.githubusercontent.com/idem2lyon/server-setup-scripts/main/adguardhome-setup.sh | sh -s -- -v
+#   curl -s -S -L -o adguardhome-setup.sh https://raw.githubusercontent.com/idem2lyon/server-setup-scripts/main/adguardhome-setup.sh
+#   chmod +x adguardhome-setup.sh
+#   sudo ./adguardhome-setup.sh
+
 # -----------------------------------------------------------------------------
 
 # -----------------------------------------------------------------------------
